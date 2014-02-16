16523e30eeff478d3e708a36fd3b72d0a7a8a229
Matt <matthew.kuzyk@gmail.com>
2014-02-16 08:09:33 -0500

    Status tipping probably works

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   addTips();
-
 });
 
 function getApiKey() {
@@ -12,39 +11,33 @@ function addTips() {
   var commentActions = $(".fsm.fwn.fcg.UFICommentActions");
   var statusActions = $("._5pcp");
   var tip = ("<a id='dogeTip'>Tip</a>   · ");
+  var tipStatus = ("<a id='dogeTipStatus'>Tip</a>   · ");
 
   commentActions.each(function() {
     $(this).prepend(tip);
   });
-    $('#dogeTip').click( function() { 
-    });
-
   statusActions.each(function() {
-    $(this).prepend(tip);
+    $(this).prepend(tipStatus);
   });
-    $('#dogeTip').click( function() { 
-    });
-
 }
 
 
 $(document).on('click', "a#dogeTip", function() {
   var e = jQuery.Event("keydown");
   e.which = 13; // # Some key code value
-  var sender = $("a.fbxWelcomeBoxName").attr('href').match("([^/]*)/$")[0];
-  console.log(sender);
-  var recipient = $(this).parent().siblings(".UFICommentContent").children(".UFICommentActorName").eq(0).attr("href").match("([^/]*)/$")[0]
+  var sender = $("a.fbxWelcomeBoxName").attr('href').match("([^/]*$)")[0];
+  var recipient = $(this).parent().siblings(".UFICommentContent").children(".UFICommentActorName").eq(0).attr("href").match("([^/]*$)")[0]
   console.log($(this));
   var action = 'http://givemethetip.com/tip';
   $(this).replaceWith("<form id='dogeForm' action=" + action +  " method='post'><input type='text' name='amount'/><input type='hidden' name='sender_user_id' value=" + sender + "/><input type='hidden' name='recipient_user_id' value=" + recipient + "/></form>");
-  //THE AUTO COMMENT IS NOT WORKING YET
   $('#dogeForm').submit(function(ev) {
     var commentBox = $(this).parent().parent().parent().parent().parent().parent().parent().parent().find('textarea');
-    // commentBox.focus();
-    //   setTimeout(3000, function() {
-    //     commentBox.html("I just tipped you Dogecoin through http://www.givemethetip.com !");
-    //     console.log(commentBox)
-    //     commentBox.trigger(e);
-    //   });
     });
+});
+$(document).on('click', "a#dogeTipStatus", function() {
+  var sender = $("a.fbxWelcomeBoxName").attr('href').match("([^/]*$)")[0];
+  var recipient = $(this).parent().siblings().eq(0).attr("href").match("([^/]*$)")[0]
+  console.log(recipient);
+  var action = 'http://givemethetip.com/tip';
+  $(this).replaceWith("<form id='dogeForm' action=" + action +  " method='post'><input type='text' name='amount'/><input type='hidden' name='sender_user_id' value=" + sender + "/><input type='hidden' name='recipient_user_id' value=" + recipient + "/></form>");
 });
