7047e90cef8557589e9f5e9bd16c8113487016c0
Matt <matthew.kuzyk@gmail.com>
2014-02-16 04:47:04 -0500

    Sending requests actually works

--- src/script.js
+++ src/script.js
@@ -19,19 +19,24 @@ function addTips() {
 
 }
 
+
 $(document).on('click', "a#dogeTip", function() {
-    var e = jQuery.Event("keydown");
-    e.which = 13; // # Some key code value
-    var recipient = $(this).parent().siblings(".UFICommentContent").children(".UFICommentActorName").eq(0).attr("href").match("([^/]*$)")[0]
-    console.log($(this));
-    var action = 'http://givemethetip.com/' + getApiKey() + '/' + recipient;
-    // $(this).replaceWith("<form id='dogeForm' action=" + action +  " method='post'><input type=text name='amount'/></form>");
-    $(this).replaceWith("<form id='dogeForm'><input type=text name='amount'/></form>");
-    //THE AUTO COMMENT IS NOT WORKING YET
-    $('#dogeForm').submit(function(ev) {
-      ev.preventDefault();
-      var commentBox = $(this).parent().parent().parent().parent().parent().parent().parent().parent().find('textarea');
-      commentBox.focus().html("I just tipped you " + $(this).val().toString() + " Dogecoin through http://www.givemethetip.com").
-      trigger(e);
+  var e = jQuery.Event("keydown");
+  e.which = 13; // # Some key code value
+  var sender = $("a.fbxWelcomeBoxName").attr('href').match("([^/]*)/$")[0];
+  console.log(sender);
+  var recipient = $(this).parent().siblings(".UFICommentContent").children(".UFICommentActorName").eq(0).attr("href").match("([^/]*)/$")[0]
+  console.log($(this));
+  var action = 'http://givemethetip.com/tip';
+  $(this).replaceWith("<form id='dogeForm' action=" + action +  " method='post'><input type='text' name='amount'/><input type='hidden' name='sender_user_id' value=" + sender + "/><input type='hidden' name='recipient_user_id' value=" + recipient + "/></form>");
+  //THE AUTO COMMENT IS NOT WORKING YET
+  $('#dogeForm').submit(function(ev) {
+    var commentBox = $(this).parent().parent().parent().parent().parent().parent().parent().parent().find('textarea');
+    // commentBox.focus();
+    //   setTimeout(3000, function() {
+    //     commentBox.html("I just tipped you Dogecoin through http://www.givemethetip.com !");
+    //     console.log(commentBox)
+    //     commentBox.trigger(e);
+    //   });
     });
 });
