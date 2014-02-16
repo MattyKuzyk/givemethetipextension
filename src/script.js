fc075b271e1761e50e1ab9b87e088b9abb164c97
Matt <matthew.kuzyk@gmail.com>
2014-02-16 09:37:54 -0500

    AJAX workin tho

--- src/script.js
+++ src/script.js
@@ -26,18 +26,25 @@ $(document).on('click', "a#dogeTip", function() {
   var e = jQuery.Event("keydown");
   e.which = 13; // # Some key code value
   var sender = $("a.fbxWelcomeBoxName").attr('href').match("([^/]*$)")[0];
-  var recipient = $(this).parent().siblings(".UFICommentContent").children(".UFICommentActorName").eq(0).attr("href").match("([^/]*$)")[0]
+  var recipient = $(this).parent().siblings(".UFICommentContent").children(".UFICommentActorName").eq(0).attr("href").match("([^/]*$)")[0];
   console.log($(this));
   var action = 'http://givemethetip.com/tip';
-  $(this).replaceWith("<form id='dogeForm' action=" + action +  " method='post'><input type='text' name='amount'/><input type='hidden' name='sender_user_id' value=" + sender + "/><input type='hidden' name='recipient_user_id' value=" + recipient + "/></form>");
+  $(this).replaceWith("<form id='dogeForm' action=" + action +  " method='post'><input type='text' name='amount'/><input type='hidden' name='sender_user_id' value='" + sender +  "'/><input type='hidden' name='recipient_user_id' value='" + recipient + "'/></form>");
   $('#dogeForm').submit(function(ev) {
-    var commentBox = $(this).parent().parent().parent().parent().parent().parent().parent().parent().find('textarea');
-    });
+    ev.preventDefault();
+    var data = $(this).serialize();
+    $.post('http://givemethetip.com/tip', data);
+  });
 });
 $(document).on('click', "a#dogeTipStatus", function() {
   var sender = $("a.fbxWelcomeBoxName").attr('href').match("([^/]*$)")[0];
-  var recipient = $(this).parent().siblings().eq(0).attr("href").match("([^/]*$)")[0]
-  console.log(recipient);
+  var recipient = $(this).parent().parent().parent().siblings().eq(0).attr("href").match("([^/]*$)")[0];
   var action = 'http://givemethetip.com/tip';
-  $(this).replaceWith("<form id='dogeForm' action=" + action +  " method='post'><input type='text' name='amount'/><input type='hidden' name='sender_user_id' value=" + sender + "/><input type='hidden' name='recipient_user_id' value=" + recipient + "/></form>");
+  console.log(recipient);
+  $(this).replaceWith("<form id='dogeStatusForm' action=" + action +  " method='post'><input type='text' name='amount'/><input type='hidden' name='sender_user_id' value='" + sender + "'/><input type='hidden' name='recipient_user_id' value='" + recipient + "'/></form>");
+  $('#dogeStatusForm').submit(function(ev) {
+    ev.preventDefault();
+    var data = $(this).serialize();
+    $.post('http://givemethetip.com/tip', data);
+  });
 });
