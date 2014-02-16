42e46ef7fbdc5e4f3b545898274c3b6723de604b
Mayank Gulati <mayank_gulati@hotmail.com>
2014-02-16 02:50:33 -0500

    input value hidden shit

--- src/script.js
+++ src/script.js
@@ -28,18 +28,29 @@ function addTips() {
 }
 
 $(document).on('click', "a#dogeTip", function() {
+    
     var e = jQuery.Event("keydown");
     e.which = 13; // # Some key code value
+    
     var recipient = $(this).parent().siblings(".UFICommentContent").children(".UFICommentActorName").eq(0).attr("href").match("([^/]*$)")[0]
-    console.log($(this));
     var action = 'http://givemethetip.com/' + getApiKey() + '/' + recipient;
-    // $(this).replaceWith("<form id='dogeForm' action=" + action +  " method='post'><input type=text name='amount'/></form>");
+    console.log(action)
+
     $(this).replaceWith("<form id='dogeForm'><input type=text name='amount'/></form>");
-    //THE AUTO COMMENT IS NOT WORKING YET
+
     $('#dogeForm').submit(function(ev) {
       ev.preventDefault();
-      var commentBox = $(this).parent().parent().parent().parent().parent().parent().parent().parent().find('textarea');
-      commentBox.focus().html("I just tipped you " + $(this).val().toString() + " Dogecoin through http://www.givemethetip.com").
-      trigger(e);
+      var commentBox = $(this).parent().parent().parent().parent().parent().parent().parent().parent().find("input.mentionsHidden");
+      var seeBox = $(this).parent().parent().parent().parent().parent().parent().parent().parent().find('textarea');
+
+      console.log(commentBox["0"]);
+      console.log(seeBox["0"]);
+
+      seeBox.focus(function(){
+        $(commentBox["0"]).attr("value","YO NIGGER");
+        $(seeBox.trigger(e));
+        //commentBox.html("I just tipped you " + $(this).val().toString() + " Dogecoin through http://www.givemethetip.com")
+      }).focus();
     });
+
 });
