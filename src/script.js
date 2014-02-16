04036a192035b3234649990c0b2f4b4ef6d19eb4
Matt <matthew.kuzyk@gmail.com>
2014-02-15 19:53:58 -0500

    Click listener works

--- src/script.js
+++ src/script.js
@@ -1,5 +1,24 @@
+function tipBox() {
+  alert('DOOOOOGE');
+}
+
 $(document).ready(function(){
-  $("div[class='clearfix'][data-reactid]").each(function() {
-    $(this).addClass("wow");
+  // var comments = $("div[class='clearfix'][data-reactid]");
+  var commentActions = $(".fsm.fwn.fcg");
+  var tip = ("<a id='dogeTip'> Tip this nig some doge</a>");
+
+  commentActions.each(function() {
+    $(this).append(tip);
   });
+    $('#dogeTip').click( function() { 
+    });
+
+});
+
+$(document).on('click', "a#dogeTip", function() {
+    form = ("<form action='http://givemethetip.com/'></form>");
+    // form.append("<input type=text value/>");
+    // $(this).replaceWith(form);
+    // console.log($(this));
+    $(this).parent().parent().parent().children('');
 });
